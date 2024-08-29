7e4a17f54261b229fd2be5838e6604f9fc3352b7
lauriane <lauriane.teyssier@gmail.com>
2024-08-29 14:06:32 +0800

    add changes in run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
-export WANDB_API_KEY=8d739e5eaa28091db300de37eb709020ff7cf27c
-python main_offline.py cexp walker rnd \
---eval_tasks run stand flip walk \
---wandb_logging False \
---seed 42 \
---z_dimension 50 \
---weighted_cml False \
---dataset_transitions 100000 \
---learning_steps 1000000 \
---cql_alpha 0.01 \
---alpha 0.1 \
---target_conservative_penalty 50 \
---lagrange True
+# export WANDB_API_KEY=8d739e5eaa28091db300de37eb709020ff7cf27c
+python main_offline.py cexp cheetah diayn \
+--eval_tasks run \
+--entity 'l_air'\ 
+# --wandb_logging False \
+# --seed 42 \
+# --z_dimension 50 \
+# --weighted_cml False \
+# --dataset_transitions 100000 \
+# --learning_steps 1000000 \
+# --cql_alpha 0.01 \
+# --alpha 0.1 \
+# --target_conservative_penalty 50 \
+# --lagrange True \
+# entity can not be used. Kexin wandb is default entity
