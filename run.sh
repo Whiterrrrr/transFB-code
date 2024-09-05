31e14869f51c959f82333df46c027f608b22ecfa
Whiterrrrr <1155173723@link.cuhk.edu.hk>
2024-09-06 03:44:17 +0800

    change run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,14 @@
-# export WANDB_API_KEY=8d739e5eaa28091db300de37eb709020ff7cf27c
-python main_offline.py cexp cheetah diayn \
---eval_tasks run \
---entity 'l_air'\ 
-# --wandb_logging False \
-# --seed 42 \
-# --z_dimension 50 \
-# --weighted_cml False \
-# --dataset_transitions 100000 \
-# --learning_steps 1000000 \
-# --cql_alpha 0.01 \
-# --alpha 0.1 \
-# --target_conservative_penalty 50 \
-# --lagrange True \
-# entity can not be used. Kexin wandb is default entity
+export WANDB_API_KEY=8d739e5eaa28091db300de37eb709020ff7cf27c
+export CUDA_VISIBLE_DEVICES=1
+python main_offline.py cexp walker rnd \
+--eval_tasks run stand flip walk \
+--wandb_logging True \
+--seed 42 \
+--z_dimension 50 \
+--weighted_cml False \
+--dataset_transitions 100000 \
+--learning_steps 5000000 \
+--cql_alpha 0.01 \
+--alpha 0.1 \
+--target_conservative_penalty 50 \
+--lagrange True
